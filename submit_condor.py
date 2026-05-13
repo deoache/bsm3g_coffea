05bf88b4883c848b8952898f2c3f1c6ca56b33e7
tomas atehortua <toatga93@gmail.com>
2026-05-13 10:17:49 +0200

    Correct wrong implementation of the label

--- submit_condor.py
+++ submit_condor.py
@@ -31,10 +31,10 @@ def submit_condor(args):
     print(f"Creating {args.workflow}-{args.year}-{args.dataset} condor file")
     
 
-    if args.label:
-        jobname = f"{args.workflow}_{args.label}_{args.dataset}"
-    else:
-        jobname = f"{args.workflow}_{args.dataset}"
+    #if args.label:
+    #    jobname = f"{args.workflow}_{args.label}_{args.dataset}"
+    #else:
+    jobname = f"{args.workflow}_{args.dataset}"
 
     # make condor and log directories
     condor_dir = Path.cwd() / "condor"
